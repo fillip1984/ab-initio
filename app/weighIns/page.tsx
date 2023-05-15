5ffd6c857308cf2cbf41c68c6823859b3e6f409d
fillip1984 <fillip1984@gmail.com>
2023-05-14 21:37:29 -0400

    liking my format for stats now

--- app/weighIns/page.tsx
+++ app/weighIns/page.tsx
@@ -14,68 +14,45 @@ const WeighIns = async () => {
 
       <div className="flex flex-col gap-2">
         {weighIns.map((weighIn) => (
-          <div
-            key={weighIn.id}
-            className="border-1 flex items-center justify-between rounded-xl bg-gray-200 p-2">
-            <div className="flex flex-col">
-              <span className="flex items-center justify-between gap-2">
-                <IoScaleOutline />
-                <span className="text-3xl">{weighIn.weight.toNumber()}</span>
+          <div key={weighIn.id} className="flex flex-col rounded-lg border-2">
+            <div className="flex items-center justify-center gap-2 bg-gray-100 p-1">
+              <BsCalendarEvent />
+              {weighIn.date.toISOString().substring(0, 10)}
+            </div>
+            <div className="flex flex-1 justify-between bg-gray-300 p-4">
+              <span className="flex flex-col items-center text-3xl">
+                <span className="text-xs uppercase text-gray-500">
+                  Weight (LBS)
+                </span>
+                {weighIn.weight.toNumber()}
+                <div className="flex items-center gap-1 text-gray-500"></div>
               </span>
-              <div className="flex items-center justify-between gap-2">
-                <div className="text-2xl">
-                  {weighIn.weightProgress.isPositive() &&
-                    !weighIn.weightProgress.isZero() && <MdTrendingUp />}
-                  {weighIn.weightProgress.isZero() && <MdTrendingFlat />}
-                  {weighIn.weightProgress.isNegative() && <MdTrendingDown />}
-                </div>
-                <div className="flex flex-col items-end text-xs">
-                  <span>
-                    {weighIn.weightProgress.toNumber()} lbs{" "}
-                    {weighIn.weightProgress.isNegative() ||
-                    weighIn.weightProgress.isZero()
-                      ? "lost"
-                      : "gained"}
-                  </span>
-                  <span>
-                    {weighIn.weightTotalChange.toNumber()} lbs{" "}
-                    {weighIn.weightTotalChange.isNegative() ||
-                    weighIn.weightTotalChange.isZero()
-                      ? "lost"
-                      : "gained"}{" "}
-                    overall
-                  </span>
-                  <span>{weighIn.weightToGoal.toNumber()} to goal</span>
-                </div>
-              </div>
+              <span className="flex flex-col items-center text-xl">
+                <span className="text-xs uppercase text-gray-500">To Date</span>
+                {weighIn.weightProgress.toNumber()}
+                {weighIn.weightProgress.isPositive() &&
+                  !weighIn.weightProgress.isZero() && <MdTrendingUp />}
+                {weighIn.weightProgress.isZero() && <MdTrendingFlat />}
+                {weighIn.weightProgress.isNegative() && <MdTrendingDown />}
+              </span>
+              <span className="flex flex-col items-center text-xl">
+                <span className="text-xs uppercase text-gray-500">Total</span>
+                {weighIn.weightTotalChange.toNumber()}
+                {weighIn.weightTotalChange.isPositive() &&
+                  !weighIn.weightTotalChange.isZero() && <MdTrendingUp />}
+                {weighIn.weightTotalChange.isZero() && <MdTrendingFlat />}
+                {weighIn.weightTotalChange.isNegative() && <MdTrendingDown />}
+              </span>
+              <span className="flex flex-col items-center text-3xl">
+                <span className="text-xs uppercase text-gray-500">To Goal</span>
+                {weighIn.weightToGoal.toNumber()}
+              </span>
             </div>
 
-            {/* {weighIn.bodyFatPercentage && (
-              <div className="flex flex-col">
-                <div className="flex items-center justify-between gap-2">
-                  <GiMuscleFat />
-                  {weighIn.bodyFatPercentage?.toString()}%
-                </div>
-                <div className="flex items-center justify-between gap-2">
-                  <div className="text-2xl">
-                    <MdTrendingDown />
-                  </div>
-                  <div className="flex flex-col items-end text-xs">
-                    <span>.25% lost</span>
-                    <span>4.12% to goal</span>
-                  </div>
-                </div>
-              </div>
-            )} */}
-
-            <div className="flex flex-col">
-              <div className="flex items-center justify-between gap-2">
-                <BsCalendarEvent />
-                {weighIn.date.toISOString().substring(0, 10)}
-              </div>
-              {/* <div className="flex items-center gap-2 text-xs">
-                13 days into journey
-              </div> */}
+            {/* TODO: add in additional stats? */}
+            <div className="flex justify-around border-t-2 border-t-gray-200 bg-gray-300 p-2">
+              <span>23% bf</span>
+              <span>28.4 BMI</span>
             </div>
           </div>
         ))}
